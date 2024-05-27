156295866e44cf9aeac314249455fd86b300a418
yang-cheng-yu <cyycvdgytuseracc@gmail.com>
2024-05-26 23:46:04 -0400

    Added tests for AddressTest

--- Project/src/test/java/AddressTest.java
+++ Project/src/test/java/AddressTest.java
@@ -1,2 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+
 public class AddressTest {
+    public void testIsPostalCodeValid1() {
+        String postalCode = "c3c3c3";
+        boolean expected = true;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
+    public void testIsPostalCodeValid2() {
+        String postalCode = "c3c 3c3";
+        boolean expected = true;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
+    public void testIsPostalCodeValid3() {
+        String postalCode = "c3c-3c3";
+        boolean expected = false;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
+    public void testIsPostalCodeValid4() {
+        String postalCode = "3c3c3c";
+        boolean expected = false;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
+    public void testIsPostalCodeValid5() {
+        String postalCode = "";
+        boolean expected = false;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
+    public void testIsPostalCodeValid6() {
+        String postalCode = null;
+        boolean expected = false;
+        boolean result = Address.isPostalCodeValid(postalCode);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
